0418d30ca5b1725e83ab86c7f9b2ceb70933fff0
Черенцов Алексей <st_aleks@rambler.ru>
2019-01-04 20:19:11 +0700

    Под задание 1.9

--- init2.sh
+++ init2.sh
@@ -1,5 +1,4 @@
-sudo ?ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
 sudo /etc/init.d/gunicorn restart
-?sudo /etc/init.d/mysql start?

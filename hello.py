f51ab456186a8737a415e2df83c1caea702dae6a
Черенцов Алексей <st_aleks@rambler.ru>
2019-01-04 21:34:32 +0700

    Signed-off-by: Черенцов Алексей <st_aleks@rambler.ru> Итоговая 1.9

--- hello.py
+++ hello.py
@@ -1,4 +1,3 @@
 def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
-encoding="utf8")]
+    return "\n".join(environ.get('QUERY_STRING').split("&"))

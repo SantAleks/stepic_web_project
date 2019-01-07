6c320157e6f4e9a0edd45e8cdd7c7118f09cc150
SantAleks <st_aleks@rambler.ru>
2019-01-07 16:22:52 +0700

    Added Django folders 2.1

--- init2.sh
+++ init2.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart

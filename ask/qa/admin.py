1ede781b86cb267af43fef8320cee6c8b5537c54
Черенцов Алексей <st_aleks@rambler.ru>
2019-01-10 20:02:41 +0700

    2.5

--- ask/qa/admin.py
+++ ask/qa/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Question, Answer
+import pytz
 
-# Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'text', 'added_at', 'author', 'rating')
+    date_hierarchy = 'added_at'
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('text', 'added_at', 'author')
+    date_hierarchy = 'added_at'
+    raw_id_fields = ('question',)
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,35 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
-class QuestionManager(models.Manager):                                          
-    def new(self):
-        return self.order_by('-added_at')                                                        
-    def popular(self):
-        return self.order_by('-rating')
-
-# Create your models here.
 
 class Question(models.Model):
-    objects = QuestionManager() 
-    title = models.CharField(default="", max_length=1024)
-    text = models.TextField(default="")
+    title = models.CharField(max_length=255)
+    text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    likes = models.ManyToManyField(User, related_name="q_to_likes")
+    author = models.ForeignKey(User, related_name="question_author")
+    likes = models.ManyToManyField(
+        User, related_name="question_like", blank=True)
+
+    class Meta:
+        ordering = ('-added_at',)
 
     def __str__(self):
         return self.title
 
-    def get_url(self):
-        return "/question/{}/".format(self.id)
+    def get_absolute_url(self):
+        return reverse('question_detail', kwargs={'pk': self.pk})
 
 
 class Answer(models.Model):
-    text = models.TextField(default="")
+    text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
-    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
-    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
+
+    class Meta:
+        ordering = ('added_at',)
 
     def __str__(self):
-        return self.text
+        return 'Answer by {}'.format(self.author)
